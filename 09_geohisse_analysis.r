33d96d53709a064f04b3167fbfe273b84bf5b8ad
jboyko <jamesboyko21@gmail.com>
2023-08-04 13:20:16 -0400

    plots and tables

--- 09_geohisse_analysis.r
+++ 09_geohisse_analysis.r
@@ -112,15 +112,30 @@ get_full_clade_result <- function(clade, model_files, recon_files){
 }
 
 # get the full results for each clade
+# result_i <- result[[16]]
+# recon_i <- recon[[16]]
 getTipPars <- function(result_i, recon_i){
   summ_results <- convertGeohisseFit2Pars(result_i)
-  turn <- summ_results$turnover
+  recon_mat <- recon_i$tip.mat[,-1]
+  rates_mat <- recon_i$rates.mat
+  recon_mat <- recon_mat/rowSums(recon_mat)
+  # turn <- summ_results$turnover
+  # ef <- setNames(vector("numeric", length(turn)), gsub("tau", "ef", names(turn)))
+  # ef[match(names(summ_results$extinction_fraction), names(ef))] <- summ_results$extinction_fraction
   rate <- rowSums(summ_results$transition_matrix)
   n_max <- summ_results$nHiddenStates * summ_results$nObservedStates
-  recon_mat <- recon_i$tip.mat[,c(2:(n_max+1))]
-  tip_turn <- colSums(turn * t(recon_mat))
+  # recon_mat <- recon_i$tip.mat[,c(2:(n_max+1))]
+  # recon_mat <- recon_mat/rowSums(recon_mat) # rescale so probs = 1
+  tip_div_mat <- apply(rates_mat, 1, function(x) colSums(x * t(recon_mat)))
+  # tip_turn <- colSums(turn * t(recon_mat))
+  # tip_ef <- colSums(ef * t(recon_mat))
   tip_rate <- colSums(rate * t(recon_mat))
-  return(list(tip_turn = tip_turn, tip_rate = tip_rate))
+  return(list(tip_turn = tip_div_mat[,1], 
+              tip_ef = tip_div_mat[,2], 
+              #tip_lambda = tip_div_mat[,3], 
+              #tip_mu = tip_div_mat[,4], 
+              #tip_net.div = tip_div_mat[,5], 
+              tip_rate = tip_rate))
 }
 
 # full_result <- get_full_clade_result(clades[1], to_load_results, to_load_recon)
@@ -134,12 +149,24 @@ get_model_avg_params <- function(full_result, return_summ = FALSE){
     return(lapply(result, convertGeohisseFit2Pars))
   }
   all_tip_rates <- mapply(getTipPars, result, recon)
-  tip_avg_turn <- tip_avg_rate <- 0
+  tip_avg_turn <- tip_avg_ef <- tip_avg_lambda <- tip_avg_mu <- tip_avg_net.div <- tip_avg_rate <- 0
   for(i in 1:ncol(all_tip_rates)){
-    tip_avg_turn <-  tip_avg_turn + all_tip_rates[,i][[1]] * model_table$AICwt[i]
-    tip_avg_rate <- tip_avg_rate + all_tip_rates[,i][[2]] * model_table$AICwt[i]
+    tip_avg_turn <-  tip_avg_turn + all_tip_rates[,i]$tip_turn * model_table$AICwt[i]
+    tip_avg_ef <-  tip_avg_ef + all_tip_rates[,i]$tip_ef * model_table$AICwt[i]
+    # tip_avg_lambda <-  tip_avg_lambda + all_tip_rates[,i]$tip_lambda * model_table$AICwt[i]
+    # tip_avg_mu <-  tip_avg_mu + all_tip_rates[,i]$tip_mu * model_table$AICwt[i]
+    # tip_avg_net.div <-  tip_avg_net.div + all_tip_rates[,i]$tip_net.div * model_table$AICwt[i]
+    tip_avg_rate <- tip_avg_rate + all_tip_rates[,i]$tip_rate * model_table$AICwt[i]
   }
-  return(data.frame(tip_avg_turn = tip_avg_turn, tip_avg_rate = tip_avg_rate))
+  area <- result[[1]]$data$area[match(result[[1]]$phy$tip.label, result[[1]]$data$species)]
+  return(data.frame(area = area, 
+                    tip_avg_turn = tip_avg_turn, 
+                    tip_avg_ef = tip_avg_ef, 
+                    # tip_avg_lambda = tip_avg_lambda,
+                    # tip_avg_mu = tip_avg_mu,
+                    # tip_avg_net.div = tip_avg_net.div,
+                    tip_avg_rate = tip_avg_rate, 
+                    row.names = result[[1]]$phy$tip.label))
 }
 
 # Preparing data - areas have to be as 0 (11 - widespread), 
@@ -148,25 +175,84 @@ get_model_avg_params <- function(full_result, return_summ = FALSE){
 
 # png("figures/pilot_turnover_rate.png", width = 1000, height = 1000)
 # par(mfrow=c(7,1))
+
+# Assuming your data.frame is named 'df'
+library(ggplot2)
+
 for(i in 1:length(clades)){
   full_result <- get_full_clade_result(clades[i], to_load_results, to_load_recon)
   if(all(is.na(full_result$model_table))){
     next
   }
-  dat <- full_result$res[[1]]$data[,2]+1
   phy <- full_result$res[[1]]$phy
   tip_correlations <- get_model_avg_params(full_result, return_summ = FALSE)
-  rownames(tip_correlations) <- full_result$res[[1]]$data[,1]
-  # hist(tip_correlations$tip_avg_turn, xlab = "Average turnover", main = clades[i], col = dat)
-  # hist(tip_correlations$tip_avg_rate, xlab = "Average rate", main = clades[i], col = dat)
   phy$root.edge <- NULL
+  # png(paste0("plots/plot_", clades[i], ".png"))
+  
+  # Turnover
   res_plm <- phylolm::phylolm(tip_avg_turn ~ tip_avg_rate, data = tip_correlations, phy = phy)
+  tip_correlations$predicted_turn <- predict(res_plm, newdata = tip_correlations)
   sig <- summary(res_plm)
-  to_add <- ifelse(sig$coefficients[2,4] < 0.05, "*", "")
-  png(paste0("plots/plot_", clades[i], ".png"))
-  plot(x = tip_correlations$tip_avg_rate, y = tip_correlations$tip_avg_turn, xlab = "Average rate", ylab = "Average turnover", main = paste0(clades[i], " ", to_add), pch = 16, col = dat)
+  main <- paste0("Turnover \n(p = ", sig$coefficients[2,4], ")")
+  a <- ggplot(tip_correlations, aes(x = tip_avg_rate, y = tip_avg_turn, color = factor(area))) +
+    geom_point() +
+    geom_line(aes(y = predicted_turn), color = "black") +
+    # geom_smooth(method = "lm", formula = y ~ x, se = FALSE, color = "red") +
+    labs(title = main, x = "tip_avg_rate", y = "tip_avg_turn") + 
+    theme(legend.position = "none") + 
+    ylim(c(0, NA))
+  
+  # Extinction Fraction
+  res_plm <- phylolm::phylolm(tip_avg_ef ~ tip_avg_rate, data = tip_correlations, phy = phy)
+  tip_correlations$predicted_turn <- predict(res_plm, newdata = tip_correlations)
+  sig <- summary(res_plm)
+  main <- paste0("Extinction Fraction \n(p = ", sig$coefficients[2,4], ")")
+  b <- ggplot(tip_correlations, aes(x = tip_avg_rate, y = tip_avg_ef, color = factor(area))) +
+    geom_point() +
+    geom_line(aes(y = predicted_turn), color = "black") +
+    # geom_smooth(method = "lm", formula = y ~ x, se = FALSE, color = "red") +
+    labs(title = main, x = "tip_avg_rate", y = "tip_avg_turn") + 
+    theme(legend.position = "none") +
+    ylim(c(0, NA))
+  
+  # Fraction versus Turnover
+  res_plm <- phylolm::phylolm(tip_avg_turn ~ tip_avg_ef, data = tip_correlations, phy = phy)
+  tip_correlations$predicted_turn <- predict(res_plm, newdata = tip_correlations)
+  sig <- summary(res_plm)
+  main <- paste0("Turn ~ Ef \n(p = ", sig$coefficients[2,4], ")")
+  c <- ggplot(tip_correlations, aes(x = tip_avg_ef, y = tip_avg_turn, color = factor(area))) +
+    geom_point() +
+    geom_line(aes(y = predicted_turn), color = "black") +
+    # geom_smooth(method = "lm", formula = y ~ x, se = FALSE, color = "red") +
+    labs(title = main, x = "tip_avg_ef", y = "tip_avg_turn") + 
+    theme(legend.position = "none") +
+    ylim(c(0, NA))
+  
+  # Box plot of 'tip_avg_rate' grouped by 'area'
+  d <- ggplot(tip_correlations, aes(x = factor(area), y = tip_avg_rate, fill = factor(area))) +
+    geom_boxplot() +
+    labs(title = "Disperal Rate", x = "Area", y = "tip_avg_rate") + 
+    theme(legend.position = "none") +
+    ylim(c(0, NA))
+  
+  e <- ggplot(tip_correlations, aes(x = factor(area), y = tip_avg_turn, fill = factor(area))) +
+    geom_boxplot() +
+    labs(title = "Turnover", x = "Area", y = "tip_avg_turn") + 
+    theme(legend.position = "none") +
+    ylim(c(0, NA))
+  
+  f <- ggplot(tip_correlations, aes(x = factor(area), y = tip_avg_ef, fill = factor(area))) +
+    geom_boxplot() +
+    labs(title = "Extinction Fraction", x = "Area", y = "tip_avg_ef") + 
+    theme(legend.position = "none") +
+    ylim(c(0, NA))
+  
+  out <- gridExtra::grid.arrange(a,b,c,d,e,f, nrow = 2, top=grid::textGrob(clades[i]))
+  ggsave(filename = paste0("plots/plots_", clades[i], ".pdf"), plot = out, width = 16, height = 8)
+  write.csv(tip_correlations, file = paste0("tables/", clades[i], "_rate_table.csv"), row.names = FALSE)
+  # plot(x = tip_correlations$tip_avg_rate, y = tip_correlations$tip_avg_turn, xlab = "Average rate", ylab = "Average turnover", main = paste0(clades[i], " ", to_add), pch = 16, col = tip_correlations$area+1)
   # res_lm <- lm(tip_correlations$tip_avg_turn ~ tip_correlations$tip_avg_rate)
-  abline(res_plm, col = "red")
-  dev.off()
+  # abline(res_plm, col = "red")
+  #   dev.off()
 }
 # dev.off()
